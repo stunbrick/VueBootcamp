c788cc141f407c9352687eb970b565d5f668c8f8
Liam Chimes <stunbrick@gmail.com>
2024-09-26 16:55:45 +0200

    transitions and animations

--- section-7-1-STARTER/src/App.vue
+++ section-7-1-STARTER/src/App.vue
@@ -6,10 +6,21 @@
     <h2 v-if="flag" key="hello">Hello!</h2>
     <h2 v-else key="bye">BYE!</h2>
   </transition>
-  -->
 <transition name="zoom" type="animation" appear>
   <h2 v-if="flag">Hello</h2>
 </transition>
+  -->
+  <transition
+  @before-enter="beforeEnter"
+  @enter="enter"
+  @after-enter="afterEnter"
+  @before-leave="beforeLeave"
+  @leave="leave"
+  @after-leave="afterLeave"
+  :css="false"
+  >
+    <h2 v-if="flag">Hey</h2>
+  </transition>
 
 
 </template>
@@ -20,6 +31,34 @@ export default {
   data() {
     return {
       flag: true,
+    }
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("before entered: ", el);
+    },
+    enter(el, done) {
+      console.log('enter', el);
+      const anim = el.animate([{ transform: "scale3d(0,0,0)"}, {}],
+          { duration: 1000, });
+      anim.onfinish = () => {
+        done();
+      }
+    },
+    afterEnter(el) {
+
+      console.log("after entered: ", el);
+    },
+    beforeLeave(el) {
+      console.log("before leave: ", el);
+    },
+    leave(el, done) {
+      console.log('leave', el);
+      const anim = el.animate([{}, { transform: "scale3d(0,0,0)"}],
+          { duration: 1000, });
+      anim.onfinish = () => {
+        done();
+      }
     }
   }
 }
